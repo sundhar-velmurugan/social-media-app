0a3ad2e1b903df80161bb04bce26c173a4cc9f33
Sundhar <sundhar811@gmail.com>
2019-11-03 18:57:57 +0530

    implementing jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -60,8 +62,17 @@ router.post(
       await user.save();
 
       // Return jsonwebtoken - necessary for logging in the user after registration
+      const payload = {
+        user: {
+          // mongoose uses an abstraction over _id property and uses id key for ObjectId
+          id: user.id
+        }
+      };
 
-      res.send('User registered');
+      jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      });
     } catch (err) {
       console.err(err.message);
       res.status(500).send('Server error');
